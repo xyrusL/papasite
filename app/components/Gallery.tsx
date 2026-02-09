e2b9a3492d25429b9ba0de21906f4f6c5abe4c58
xyrusL <91442589+xyrusL@users.noreply.github.com>
2026-02-09 15:47:13 +0800

    chore(gallery): apply latest gallery component updates

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -30,7 +30,7 @@ export default function Gallery() {
     return (
         <section className="py-24 relative overflow-hidden">
             {/* Background Glow */}
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-yellow-500/5 rounded-full blur-3xl"></div>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[70vw] h-[70vw] max-w-[500px] max-h-[500px] bg-yellow-500/5 rounded-full blur-3xl"></div>
 
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
                 <motion.div
